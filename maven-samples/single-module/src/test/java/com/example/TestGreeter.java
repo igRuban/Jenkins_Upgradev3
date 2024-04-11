27d03263d96584f5d19a8eb9dd9cc63fd9b6ed26
Ruban <126575183+igRuban@users.noreply.github.com>
2024-04-11 16:21:06 +0530

    Update TestGreeter.java

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -28,12 +28,7 @@ public class TestGreeter {
   }
   
 
-  @Test
-  public void greetShouldIncludeGreetingPhrase() {
-    String someone = "World";
 
-    assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
-  }
 
     @Test
   public void greetShouldIncludeGreetingMessage() {
